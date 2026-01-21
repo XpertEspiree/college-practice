0c6225e6ff5cc59dc96c311bd71b43fc0edd3109
Bad Coder <xpertespire@gmail.com>
2026-01-21 08:57:49 +0545

    Add files via upload

---
+++ atm.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main(){
+	int balance=0;
+	int spin=1234, pin;
+	printf("Enter pin code: ");
+	scanf("%d", &pin);
+	
+	if(spin==pin){
+		printf("Menu\n");
+		printf("1.Balance Inquiry. \n2.Cash Withdrawl. \n3.Deposit. \n4.Exits. \nSelect menu: ");
+		scanf("%d", &menu);
+		
+		
+		switch(menu){
+			case 1:
+				printf("Your current balance is = %d", balance);
+				break;
+				
+				case 2:
+				printf("Enter amount: ");
+				scanf("%d", &amount);
+				
+				if(balance<amount){
+					printf("Insufficient balance.");
+				}else{
+					balance-=amount;
+					printf("Withdrawl amount = %d\n", amount);
+					printf("Remaining balance = %d\n", balance);
+				}
+				break;
+				
+				case 3:
+					printf("Enter amount: ");
+					scanf("%d", amount);
+					balance=amount;
+					printf("Total balance = %d", balance);
+					printf("Amount deposited sucessfully\n");
+					break;
+					
+					case 4:
+						printf("Exit");
+						break;
+						default:
+							printf("Invalid menu selection. \n")
+				
+				
+			
+				}
+		
+		
+	}else{
+		printf("Incorrect pin.");
+	}
+}
+return 0;
