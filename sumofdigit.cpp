4e7281dc9c36989d0c74bdac5cab966cc70283d2
Bad Coder <xpertespire@gmail.com>
2026-01-25 09:05:07 +0545

    Add files via upload

---
+++ sumofdigit.cpp
@@ -0,0 +1,18 @@
+//WAP in c to find sum of digits of a number entered by the user
+//input:123
+//output:6
+#include<stdio.h>
+int main(){
+	int number, digit, sum=0;
+	
+	printf("Enter a number: ");
+	scanf("%dd", &number);
+	
+	while(number>0){
+		digit=number%10;
+		sum=sum+digit;
+		number=number/10;
+	}
+	printf("Sum of digit of number %d is = %d", number, sum);
+	return 0;
+}
